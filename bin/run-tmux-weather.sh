b2cdf924d0aec692117a6ece6c524a0754e2fbb4
Jacob Lewallen <jlewallen@gmail.com>
2018-03-08 16:23:57 -0800

    Pass known weather Wifi AP name to wifi tool

--- bin/run-tmux-weather.sh
+++ bin/run-tmux-weather.sh
@@ -4,4 +4,4 @@ tmux new -d -s my-session 'echo core; ./monitor.sh ~/devices/core fk-core.bin co
             new-window -d 'echo weather; ./monitor.sh ~/devices/weather fk-weather-module.bin weather' \; \
             new-window -d 'echo core; touch core.log && tail -f core.log' \; \
             new-window -d 'echo weather; touch weather.log && tail -f weather.log' \; \
-            new-window -d 'sudo ~/tools/bin/fk-wifi-tool --data-directory ~/data --device wlan1 --wpa-socket /run/wpa_supplicant_wlan1 --device-address 192.168.2.1 --upload-data' \;
+            new-window -d 'sudo ~/tools/bin/fk-wifi-tool --data-directory ~/data --device wlan1 --wpa-socket /run/wpa_supplicant_wlan1 --device-address 192.168.2.1 --upload-data --network FK-AASjCwAc0sU' \;
